bbb9f8857b72f0d6eaaee6ddbfff439bf8038fe5
Finn <mailtorufina@gmail.com>
2023-04-19 11:30:52 +0100

    add header code

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className={styles.logo}>
+        <img src="" />
+      </div>
+      <nav>
+        <ul className={styles.navList}>
+          <li>
+            <a link="#"></a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
