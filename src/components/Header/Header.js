ee5259a6b021500d8b58190669153375ed3a04c8
Finn <mailtorufina@gmail.com>
2023-04-26 18:56:53 +0100

    add categories text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,16 +5,33 @@ const Header = () => {
     <header className={styles.header}>
       <div className={styles.logo}>
         <img src="" />
+        <div className={styles.name}></div>
       </div>
       <nav className={styles.nav}>
         <ul className={styles.navList}>
+          <div className={styles.dropdown}>
+            <p className={styles.dropbtn}>CATEGORIES</p>
+            <div className={styles.dropdownContent}>
+              <ul>
+                <li>
+                  <a href="#">Mail</a>
+                </li>
+                <li>
+                  <a href="#">LinkedIn</a>
+                </li>
+                <li>
+                  <a href="#">Twitter</a>
+                </li>
+              </ul>
+            </div>
+          </div>
           <li>
             <a link="#">About</a>
           </li>
           <li>
-            <div class={styles.dropdown}>
-              <p class={styles.dropbtn}>Contacts</p>
-              <div class={styles.dropdownContent}>
+            <div className={styles.dropdown}>
+              <p className={styles.dropbtn}>Contacts</p>
+              <div className={styles.dropdownContent}>
                 <ul>
                   <li>
                     <a href="#">Mail</a>
