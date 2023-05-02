7e9d0ba80075a4f3344fa50071a1ce96ddfcb847
Finn <mailtorufina@gmail.com>
2023-05-02 12:41:19 +0100

    extract nav bar code into own folder

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,58 +1,7 @@
 import styles from "./Header.module.css";
 
 const Header = () => {
-  return (
-    <header className={styles.header}>
-      <nav className={styles.navList}>
-        <div className={styles.logo}>
-          <img src="" />
-          <h2 className={styles.name}></h2>
-        </div>
-        <ul className={styles.navLinks}>
-          <div className={styles.dropdown}>
-            <p className={styles.dropbtn}>CATEGORIES</p>
-            <div className={styles.dropdownContent}>
-              <ul>
-                <li>
-                  <a href="#">New to tech</a>
-                </li>
-                <li>
-                  <a href="#">Reveal</a>
-                </li>
-                <li>
-                  <a href="#">Projects</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <div className={styles.dropdown}>
-              <p className={styles.dropbtn}>Contacts</p>
-              <div className={styles.dropdownContent}>
-                <ul>
-                  <li>
-                    <a href="#">
-                      <i class="fa-sharp fa-light fa-envelope-open"></i>
-                      Mail
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">LinkedIn</a>
-                  </li>
-                  <li>
-                    <a href="#">Twitter</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+  return <header className={styles.header}></header>;
 };
 
 export default Header;
