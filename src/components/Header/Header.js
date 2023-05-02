770c014f571c02077430db8a640439cc2b38c2be
Finn <mailtorufina@gmail.com>
2023-05-02 12:43:59 +0100

    import Nav.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,12 @@
 import styles from "./Header.module.css";
+import Nav from "../Nav/Nav";
 
 const Header = () => {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <Nav />
+    </header>
+  );
 };
 
 export default Header;
