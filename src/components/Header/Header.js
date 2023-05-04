9b85c5d285d9f29c573e375e8fc38ad53d794ed0
Finn <mailtorufina@gmail.com>
2023-05-04 22:11:11 +0100

    import Hero Component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import styles from "./Header.module.css";
 import Nav from "../Nav/Nav";
+import HeroComponent from "../HeroComponent/HeroComponent";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <Nav />
+      <HeroComponent />
     </header>
   );
 };
