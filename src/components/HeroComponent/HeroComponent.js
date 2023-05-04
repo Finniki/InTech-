00a6653ea8cfce4ca7eb80a8b6d7b2c1b1c60328
Finn <mailtorufina@gmail.com>
2023-05-04 23:49:21 +0100

    import and render code image

--- src/components/HeroComponent/HeroComponent.js
+++ src/components/HeroComponent/HeroComponent.js
@@ -1,10 +1,13 @@
 import styles from "./HeroComponent.module.css";
+import codeImage from "../../assets/images/code.jpg";
 
 const HeroComponent = () => {
   return (
     <div className={styles.heroArea}>
       <div className={styles.textArea}>INTECH</div>
-      <div className={styles.imageArea}></div>
+      <div className={styles.imageArea}>
+        <img src={codeImage} />
+      </div>
     </div>
   );
 };
