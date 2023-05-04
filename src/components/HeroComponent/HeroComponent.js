c96c6e233e9ccdc8490eb738beb5ed80b2ce1f82
Finn <mailtorufina@gmail.com>
2023-05-04 23:23:37 +0100

    move hero text above image

--- src/components/HeroComponent/HeroComponent.js
+++ src/components/HeroComponent/HeroComponent.js
@@ -3,8 +3,8 @@ import styles from "./HeroComponent.module.css";
 const HeroComponent = () => {
   return (
     <div className={styles.heroArea}>
-      <div className={styles.imageArea}></div>
       <div className={styles.textArea}>INTECH</div>
+      <div className={styles.imageArea}></div>
     </div>
   );
 };
