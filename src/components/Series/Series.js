f2e04a56ddd4183423d3a1489d80546db1a1586c
Finn <mailtorufina@gmail.com>
2023-05-07 14:09:55 +0100

    add series

---
+++ src/components/Series/Series.js
@@ -0,0 +1,11 @@
+import styles from "./Series.module.css";
+
+const Series = () => {
+  return (
+    <main>
+      <section className="section cloneProjectsSection"></section>
+      <section className="section newToTechSection"></section>
+      <section className="section jsProjectsSection"></section>
+    </main>
+  );
+};
